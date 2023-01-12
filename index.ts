b7d318b0067d3cddf3c3e434e02505a3d80b4fef
Aloys Berger <aloys.berger@technichegroup.com>
2023-01-12 10:42:20 +1000

    fix(notes,-index): Fix minors bug in the notes and index
    - Fix the link of notes, the extension should be stripped,
    - Fix the writing of files, it was overwritting the source instead of
      writing to the destination

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import * as path from "https://deno.land/std@0.171.0/path/mod.ts";
-import { BLOG_DIR, VAULT_DIR } from "./config.ts";
-import { getAllProcessedNotes } from "./notes.ts";
-import { Note } from "./types.ts";
 
-await main()
+import { BLOG_DIR, VAULT_DIR } from "./config.ts";
+
+import { Note } from "./types.ts";
+import { getAllProcessedNotes } from "./notes.ts";
+
+await main();
 
 async function main() {
    confirm(`Using the following directories:
@@ -13,19 +15,19 @@ async function main() {
 `);
 
    try {
-      await prepareDestDirectory()
-      const notes = await getAllProcessedNotes(VAULT_DIR)
-      await publishNotes(notes)
+      await prepareDestDirectory();
+      const notes = await getAllProcessedNotes(VAULT_DIR);
+      await publishNotes(notes);
+      console.log("note publish");
    } catch (e) {
-      console.error("Something went wrong", e)
+      console.error("Something went wrong", e);
    }
 }
-
 
 async function publishNotes(notes: Note[]) {
    await Promise.all(
       notes.map((note) =>
-         Deno.writeTextFile(note.filePath, note.content)
+         Deno.writeTextFile(path.join(BLOG_DIR, note.title), note.content)
       )
    );
 }
@@ -33,10 +35,8 @@ async function publishNotes(notes: Note[]) {
 async function prepareDestDirectory() {
    try {
       Deno.readDirSync(BLOG_DIR);
-      Deno.removeSync(path.join(BLOG_DIR, '*'))
+      Deno.removeSync(path.join(BLOG_DIR, "*"));
    } catch {
-      Deno.mkdirSync(BLOG_DIR, { recursive: true })
+      Deno.mkdirSync(BLOG_DIR, { recursive: true });
    }
 }
-
-
--- notes.ts
+++ notes.ts
@@ -1,96 +1,103 @@
-import { parse } from "https://deno.land/std@0.171.0/encoding/yaml.ts";
-import { basename, join } from 'https://deno.land/std@0.171.0/path/mod.ts';
 import { Frontmatter, Note } from "./types.ts";
+import { basename, join } from "https://deno.land/std@0.171.0/path/mod.ts";
+
+import { parse } from "https://deno.land/std@0.171.0/encoding/yaml.ts";
 
 export async function getAllProcessedNotes(directory: string): Promise<Note[]> {
-    const notes: Note[] = [];
-    await findNotesInDirectoryRecursively(directory, notes);
-    const processedNotes = await replaceWikilinks(notes);
-    return processedNotes;
+   const notes: Note[] = [];
+   await findNotesInDirectoryRecursively(directory, notes);
+   const processedNotes = await replaceWikilinks(notes);
+   return processedNotes;
 }
 
-
 async function findNotesInDirectoryRecursively(
-    directory: string,
-    notesAccumulator: Note[]
+   directory: string,
+   notesAccumulator: Note[]
 ): Promise<Note[]> {
-    const subdirs = await Deno.readDir(directory);
+   const subdirs = await Deno.readDir(directory);
 
-    for await (const subdir of subdirs) {
-        const subdirPath = join(directory, subdir.name);
-        if (subdir.isDirectory) {
-            await findNotesInDirectoryRecursively(subdirPath, notesAccumulator);
-        }
-        const parsedNote = await parseFileIntoNote(subdirPath);
-        if (parsedNote) {
-            notesAccumulator.push(parsedNote);
-        }
-    }
+   for await (const subdir of subdirs) {
+      const subdirPath = join(directory, subdir.name);
+      if (subdir.isDirectory) {
+         await findNotesInDirectoryRecursively(subdirPath, notesAccumulator);
+      }
+      const parsedNote = await parseFileIntoNote(subdirPath);
+      if (parsedNote) {
+         notesAccumulator.push(parsedNote);
+      }
+   }
 
-    return notesAccumulator;
+   return notesAccumulator;
 }
 
 async function parseFileIntoNote(filePath: string): Promise<Note | null> {
-    let content: string;
-    try {
-        content = await Deno.readTextFile(filePath);
-    } catch (e) {
-        return null;
-    }
+   let content: string;
+   try {
+      content = await Deno.readTextFile(filePath);
+   } catch (e) {
+      return null;
+   }
 
-    try {
-        const rawFrontmatter = parse(content.split("---")[1]) as Frontmatter;
-        const frontmatter = {
-            ...rawFrontmatter,
-            created_at: new Date(rawFrontmatter.created_at),
-            last_modified_at: new Date(rawFrontmatter.last_modified_at),
-            slug:
-                rawFrontmatter?.slug ??
-                rawFrontmatter.title.split(" ").join("-").toLowerCase(),
-        };
-        return {
-            title: basename(filePath),
-            filePath,
-            content,
-            frontmatter,
-        };
-    } catch (e) {
-        console.warn(
-            `${basename(
-                filePath
-            )} doesn't have a frontmatter and won't be published.`
-        );
-        return null;
-    }
+   try {
+      const rawFrontmatter = parse(content.split("---")[1]) as Frontmatter;
+      const frontmatter = {
+         ...rawFrontmatter,
+         created_at: new Date(rawFrontmatter.created_at),
+         last_modified_at: new Date(rawFrontmatter.last_modified_at),
+         slug:
+            rawFrontmatter?.slug ??
+            rawFrontmatter.title.split(" ").join("-").toLowerCase(),
+      };
+      return {
+         title: basename(filePath),
+         filePath,
+         content,
+         frontmatter,
+      };
+   } catch (e) {
+      console.warn(
+         `${basename(
+            filePath
+         )} doesn't have a frontmatter and won't be published.`
+      );
+      return null;
+   }
 }
 
 export function replaceWikilinks(notes: Note[]): Note[] {
-    return notes.map((note) => {
-        const { content } = note;
-        const lines = content.split("\n");
-        note.content = lines.map((line) => {
+   return notes.map((note) => {
+      const { content } = note;
+      const lines = content.split("\n");
+      note.content = lines
+         .map((line) => {
             const regexp = /\[\[.+?\|?.*?\]\]/g;
-            const wikilinks = line.match(regexp)
+            const wikilinks = line.match(regexp);
 
             if (!wikilinks) {
-                return line;
+               return line;
             }
             const newLine = line.replace(regexp, processLink);
             return newLine;
 
             function processLink(link: string): string {
-                const [file, title] = link.slice(2, -2).split("|");
-                const linkedNote = notes.find((note) => note.title === file);
-                if (!linkedNote) {
-                    console.log(`${note.filePath} has a link to '${file}' that we could not resolve. The link has been ignored.`)
-                    const replace = `${title ?? file}`;
-                    return replace
-                }
-                const replace = `[${title ?? file}](../${linkedNote.frontmatter.slug}.md)`;
-                return replace
+               const [file, title] = link.slice(2, -2).split("|");
+               const linkedNote = notes.find(
+                  (note) => note.title === `${file}.md`
+               );
+               if (!linkedNote) {
+                  console.log(
+                     `${note.filePath} has a link to '${file}' that we could not resolve. The link has been ignored.`
+                  );
+                  const replace = `${title ?? file}`;
+                  return replace;
+               }
+               const replace = `[${title ?? file}](./${
+                  linkedNote.frontmatter.slug
+               })`;
+               return replace;
             }
-        }).join('\n');
-        return note;
-    })
+         })
+         .join("\n");
+      return note;
+   });
 }
-
