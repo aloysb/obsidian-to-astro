0dd1055829ae34d56d7e2947d0f2da589e7c7d07
Aloys Berger <alo@reaktive.com>
2023-02-07 20:48:37 +1000

    feat(Many!): This includes commit many different aspects: add backup, review main function, add utility to prepare folders, add tests

--- lib/note.ts
+++ lib/note.ts
@@ -1,9 +1,9 @@
-import { Emitter } from "./eventEmitter.ts";
-import { blogSchema } from "./schema.ts";
 import { parse } from "https://deno.land/std@0.171.0/encoding/yaml.ts";
 import { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
+import { Emitter } from "./eventEmitter.ts";
+import { blogSchema } from "./schema.ts";
 export interface NoteProps {
-   filePath: string;
+  filePath: string;
 }
 export type Frontmatter = z.infer<typeof blogSchema>;
 
@@ -11,62 +11,52 @@ export type Frontmatter = z.infer<typeof blogSchema>;
  * Class representing the parsed version of a note.
  */
 export class Note {
-   readonly filePath: string;
-   readonly rawFile: string;
-   readonly frontmatter: Frontmatter | null;
-   private handlers: ((note: this) => void)[] = [];
+  readonly filePath: string;
+  readonly rawFile: string;
+  readonly frontmatter: Frontmatter | null;
 
-   /**
-    * Create a note
-    * @param filePath the path of file
-    * @param handlers an array of handler to execute when processing the note
-    * @param onCreatedNote a hook to run a function on note creation
-    */
-   constructor(
-      filePath: string,
-      handlers: ((note: Note) => void)[] = [],
-      onNoteCreatedEmitter: Emitter<Note>
-   ) {
-      this.filePath = filePath;
-      this.rawFile = Deno.readTextFileSync(filePath);
-      this.frontmatter = this.parseFrontmatter();
-      this.handlers = handlers;
-      onNoteCreatedEmitter.emit(this);
-   }
-   /**
-    * Return the raw content of the note, as is.
-    * This does not include frontmatter.
-    */
-   public get rawContent(): string | null {
-      try {
-         return this.rawFile.split("---")[2].trim();
-      } catch {
-         return null;
-      }
-   }
+  /**
+   * Create a note
+   * @param filePath the path of file
+   * @param handlers an array of handler to execute when processing the note
+   * @param onCreatedNote a hook to run a function on note creation
+   */
+  constructor(
+    filePath: string,
+    onNoteCreatedEmitter: Emitter<Note>,
+  ) {
+    this.filePath = filePath;
+    this.rawFile = Deno.readTextFileSync(filePath);
+    this.frontmatter = this.parseFrontmatter();
+    onNoteCreatedEmitter.emit(this);
+  }
+  /**
+   * Return the raw content of the note, as is.
+   * This does not include frontmatter.
+   */
+  public get rawContent(): string | null {
+    try {
+      return this.rawFile.split("---")[2].trim();
+    } catch {
+      return null;
+    }
+  }
 
-   /**
-    *  Parse the file frontmatter. Returns null if there is no frontmatter
-    *  @private
-    */
-   private parseFrontmatter(): Frontmatter | null {
-      try {
-         const rawFrontmatter = this.rawFile.split("---")[1] as string;
-         const frontmatter = parse(rawFrontmatter) as Frontmatter;
-         return {
-            ...frontmatter,
-            last_modified_at: new Date(frontmatter.last_modified_at),
-            created_at: new Date(frontmatter.created_at),
-         };
-      } catch {
-         return null;
-      }
-   }
-
-   /**
-    * Process the note, as per handlers;
-    */
-   public process() {
-      this.handlers.forEach((handler) => handler(this));
-   }
+  /**
+   *  Parse the file frontmatter. Returns null if there is no frontmatter
+   *  @private
+   */
+  private parseFrontmatter(): Frontmatter | null {
+    try {
+      const rawFrontmatter = this.rawFile.split("---")[1] as string;
+      const frontmatter = parse(rawFrontmatter) as Frontmatter;
+      return {
+        ...frontmatter,
+        last_modified_at: new Date(frontmatter.last_modified_at),
+        created_at: new Date(frontmatter.created_at),
+      };
+    } catch {
+      return null;
+    }
+  }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,11 @@
 // import { Note } from "./types.ts";
 import { join } from "https://deno.land/std@0.171.0/path/mod.ts";
+import { crypto } from "https://deno.land/std@0.177.0/crypto/crypto.ts";
+import {
+  copySync,
+  emptyDirSync,
+} from "https://deno.land/std@0.177.0/fs/mod.ts";
+import * as logger from "https://deno.land/std@0.177.0/log/mod.ts";
 
 // export async function getAllProcessedNotes(directory: string): Promise<Note[]> {
 //    const notes: Note[] = await findNotesInDirectoryRecursively(directory);
@@ -20,7 +26,7 @@ type Options = {
  *
  * @param directory the directory to search
  * @param options options to filter the result. So far, the sole option is to provide a regexp to match.
- * @returns anarray of filepath for the files matching the search options in the directory provided
+ * @returns an array of filepath for the files matching the search options in the directory provided
  */
 export async function findFilesRecursively(
   directory: string,
@@ -41,6 +47,53 @@ export async function findFilesRecursively(
     }
   }
   return files;
+}
+
+/**
+ * Prepare a backup of both the source and the destination folders
+ * @param sourceDir source dir filepath
+ * @param destinationDir  destination dir filepath
+ * @param backupDir the location of the backups
+ */
+export function prepareBackups(
+  sourceDir: string,
+  destinationDir: string,
+  backupDir: string,
+): string {
+  // Create a random backup dir
+  const uniqueBackupDir = join(
+    backupDir,
+    new Date().toDateString(),
+    crypto.randomUUID(),
+  );
+  prepareDestDirectory(uniqueBackupDir);
+
+  try {
+    copySync(sourceDir, join(uniqueBackupDir, "source"), { overwrite: true });
+    copySync(destinationDir, join(uniqueBackupDir, "destination"), {
+      overwrite: true,
+    });
+    logger.info(
+      `"Backup successful. \n Backup directory: ${uniqueBackupDir}`,
+    );
+    return uniqueBackupDir;
+  } catch (e) {
+    logger.error(`Failed to prepare backup: \n ${e}`);
+    throw new Error("Fail to prepare backups");
+  }
+}
+
+/**
+ * Prepare the destination directory:
+ * Empty the directory of all files or create a new directory at the given path
+ * @param dirPath the path of the directory to empty
+ */
+export function prepareDestDirectory(dirPath: string) {
+  try {
+    emptyDirSync(dirPath);
+  } catch {
+    Deno.mkdirSync(dirPath, { recursive: true });
+  }
 }
 
 // async function parseFileIntoNote(filePath:string): Promise<Note | null> {
