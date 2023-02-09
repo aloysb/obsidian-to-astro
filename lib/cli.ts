3a84e7fb2914c13d4645bf46351321859511ee46
Aloys Berger <aloys.berger@technichegroup.com>
2023-02-09 18:07:32 +1000

    feat(CLI): Improve the class, add test cases as required

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,12 +1,19 @@
+import * as logger from "https://deno.land/std@0.177.0/log/mod.ts";
+
+import {
+  findFilesRecursively,
+  prepareBackups,
+  prepareDestDirectory,
+} from "./utils.ts";
+
+import { Config } from "./config.ts";
+import { Emitter } from "./eventEmitter.ts";
+import { LinkManager } from "./linkManager.ts";
+import { Note } from "./note.ts";
+import { join } from "https://deno.land/std@0.177.0/path/mod.ts";
 import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
 
-const flags = parse(Deno.args, {
-   boolean: ["help", "publish"],
-   string: ["source", "blog"],
- });
-
-
-   export const welcomeMessage = `
+export const welcomeMessage = `
     Hey there! 
     This is how you use this tool:
     
@@ -21,17 +28,119 @@ const flags = parse(Deno.args, {
     Thank you!
     Alo.
   `;
- 
+
+export const missingArgument = `
+   It looks like you have set the --source OR --blog argument, but not both.
+   If you wish to use the manual mode, you must provide both.
+  `;
+
 /**
  * The Cli class is in charge of interacting with the user.
  * It is the main entry point that will run the right command depending on the user input
  */
-export class Cli{
-   /**
-    * handleCommand process the user command and trigger the right actions
-    */
-   public static handleCommand(arg?: typeof flags ){
-      console.log(welcomeMessage);
-   }
+export class Cli {
+  private constructor() {
+  }
+  /**
+   * handleCommand process the user command and trigger the right actions
+   */
+  public static async handleCommand(args?: typeof Deno.args) {
+    const self = new Cli();
+
+    if (!args) {
+      self.displayWelcomeMessage();
+      return;
+    }
+
+    const flags = parse(args, {
+      boolean: ["help", "publish"],
+      string: ["source", "blog"],
+    });
+
+    if (flags.help) {
+      self.displayWelcomeMessage();
+      return;
+    }
+
+    if (flags.publish) {
+      if (flags.source && flags.blog) {
+        Config.initialize({
+          type: "cli",
+          values: { sourceDir: flags.source, blogDir: flags.blog },
+        });
+      } else if (flags.source || flags.blog) {
+        console.log(missingArgument);
+        return;
+      } else {
+        Config.initialize({ type: "integrated" });
+      }
+
+      await self.publish();
+    }
+
+    return;
+  }
+
+  /**
+   * Simply display the welcome/help message!
+   */
+  private displayWelcomeMessage() {
+    console.log(welcomeMessage);
+  }
+
+  /**
+   * This is the MAIN function
+   */
+  private async publish() {
+    const { sourceDir, blogDir } = Config.retrieve();
+    const shouldProceed = confirm(`Using the following directories:
+    - Blog: ${blogDir},
+    - Vault: ${sourceDir},
+    Do you want to proceed?
+`);
+
+    if (!shouldProceed) {
+      console.log("Cancelling. Not post published");
+      return;
+    }
+    try {
+      prepareBackups(
+        sourceDir,
+        blogDir,
+        join(Deno.env.get("HOME") ?? ".vault2blog/", "/.vault2blog/backups"),
+      );
+    } catch (e) {
+      throw Error(e);
+    }
+
+    const notes = [];
+    const linkManager = new LinkManager();
+
+    // Create a note created emitter
+    const onNoteCreatedEmitter = new Emitter<Note>();
+
+    // Registers handlers at note creation
+    onNoteCreatedEmitter.on(linkManager.registerNote.bind(linkManager));
+    onNoteCreatedEmitter.on((note) =>
+      logger.info(`${note.frontmatter?.title} created`)
+    );
+
+    // Get all notes
+    const noteFilePaths = await findFilesRecursively(sourceDir, {
+      match: new RegExp(".*.md$"),
+    });
+
+    // Create all notes
+    for (const filePath of noteFilePaths) {
+      notes.push(new Note(filePath, onNoteCreatedEmitter));
+    }
+
+    // Process notes once all created
+    for (const note of notes) {
+      linkManager.replaceWikiLinks(note);
+    }
+
+    // Prepare destination directory
+    prepareDestDirectory(blogDir);
+  }
 }
- 
--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,21 +1,68 @@
-import { Cli, welcomeMessage } from "../lib/cli.ts";
+import { Cli, missingArgument, welcomeMessage } from "../lib/cli.ts";
 import {
-   assertSpyCall,
-   assertSpyCallArgs,
-   assertSpyCalls,
-   spy,
-} from "https://deno.land/std@0.173.0/testing/mock.ts";
+  assertSpyCall,
+  assertSpyCallArgs,
+  Spy,
+  spy,
+  stub,
+} from "https://deno.land/std@0.177.0/testing/mock.ts";
 import {
-   beforeEach,
-   describe,
-   it,
+  afterEach,
+  beforeEach,
+  describe,
+  it,
 } from "https://deno.land/std@0.173.0/testing/bdd.ts";
 
-describe("cli" ,() => {
+import { Config } from "../lib/config.ts";
+import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 
-   it("display a help/welcome message by default", () => {
-      const consoleSpy = spy(console, "log")
-      Cli.handleCommand(undefined);
-      assertSpyCallArgs(consoleSpy, 0, [welcomeMessage])
-   })
- })
+describe("cli", () => {
+  let consoleSpy: Spy<Console>;
+  beforeEach(() => {
+    Deno.env.set("ENV_MODE", "TEST");
+    consoleSpy = spy(console, "log");
+  });
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it("displays a help/welcome message by default", () => {
+    Cli.handleCommand(undefined);
+    assertSpyCallArgs(consoleSpy, 0, [welcomeMessage]);
+  });
+
+  it("displays the welcome/help message if the help flag is set to true", () => {
+    Cli.handleCommand(["--help"]);
+    assertSpyCallArgs(consoleSpy, 0, [welcomeMessage]);
+  });
+
+  it("should run the configuration in manual mode if a source and a destination directories are provided", () => {
+    const promptStub = stub(window, "confirm", () => false);
+    const SOURCE = "/my/path/source";
+    const BLOG = "/my/path/blog";
+    Cli.handleCommand(["--publish", "--source", SOURCE, "--blog", BLOG]);
+
+    assertEquals(Config.retrieve().blogDir, BLOG);
+    assertEquals(Config.retrieve().sourceDir, SOURCE);
+
+    promptStub.restore();
+  });
+  it("should display an error message if I only provide the source or the blog path, but not both", () => {
+    const SOURCE = "/my/path/source";
+    const BLOG = "/my/path/blog";
+    Cli.handleCommand(["--publish", "--source", SOURCE]);
+    Cli.handleCommand(["--publish", "--blog", BLOG]);
+
+    assertSpyCallArgs(consoleSpy, 0, [missingArgument]);
+    assertSpyCallArgs(consoleSpy, 1, [missingArgument]);
+  });
+});
+it("should runs the configuration in integrated mode if neither the source of the blog path are provided", () => {
+  const initializeSpy = spy(Config, "initialize");
+  const promptStub = stub(window, "confirm", () => false);
+  Cli.handleCommand(["--publish"]);
+  // @ts-ignore: I can't figure out the Spy generic
+  assertSpyCall(initializeSpy, 0, [{ type: "integrated" }]);
+  initializeSpy.restore();
+  promptStub.restore();
+});
